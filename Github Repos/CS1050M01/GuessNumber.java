ab71e94500b1ea7d2487cf1f19eba05c80a8de68
fquin404 <corrupted.darkwraith@gmail.com>
2026-02-09 22:04:40 -0700

    meow

--- Github Repos/CS1050M01/GuessNumber.java
+++ Github Repos/CS1050M01/GuessNumber.java
@@ -1,37 +1,37 @@
-import java.util.Scanner;
+import java.util.Scanner; //imports scanner
 
-public class GuessNumber
+public class GuessNumber //declares class
 {
 	public static void main(String[] args)
 	{		
-		final int RANDOM_MULTIPLIER = 100;
+		final int RANDOM_MULTIPLIER = 100; //limits to 100
 
-		int number = (int) (Math.random() * RANDOM_MULTIPLIER) + 1;
+		int number = (int) (Math.random() * RANDOM_MULTIPLIER) + 1; //picks number
 
-		Scanner keyboardInput = new Scanner(System.in);
-		
-		System.out.println("Guess a magic number between 0 and 100");
+		try (Scanner keyboardInput = new Scanner(System.in)) {
+			System.out.println("Guess a magic number between 0 and 100"); //user prompt
 
-		int guess = -1;
-		while (guess != number)
-		{
-
-			System.out.print("\nEnter your guess: ");
-			guess = keyboardInput.nextInt();
-
-			if (guess == number) 
+			int guess = -1; //picks number - 100
+			while (guess != number) //holds guess
 			{
-				System.out.println("Yes, the number is " + number);
-			}
-			else if (guess > number)
-			{
-				System.out.println("Your guess is too high");
-			}
-			else
-			{
-				System.out.println("Your guess is too low");
+
+				System.out.print("\nEnter your guess: "); //prompts user to keyboard
+				guess = keyboardInput.nextInt();
+
+				if (guess == number) 
+				{
+					System.out.println("Yes, the number is " + number); //win
+				}
+				else if (guess > number)
+				{
+					System.out.println("Your guess is too high"); //loss+
+				}
+				else
+				{
+					System.out.println("Your guess is too low"); //loss-
+				}
 			}
 		}
 
-	}//end of main
-}//end of class
+	}//end of main code
+}//end of class code
