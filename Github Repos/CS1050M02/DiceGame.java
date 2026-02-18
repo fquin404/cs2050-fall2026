404150ea46ab426c7b89d21021826ff0abe695d7
fquin404 <corrupted.darkwraith@gmail.com>
2026-02-18 13:30:49 -0700

    meow

---
+++ Github Repos/CS1050M02/DiceGame.java
@@ -0,0 +1,42 @@
+
+// ADD CODE to import Scanner class
+
+public class DiceGame
+{
+
+	public static void main(String[] args)
+	{
+
+		{
+			double max = 6;
+			double min = 1;
+			double d1 = (int) (Math.random() * ((max - min) + 1)) + min;
+			double d2 = (int) (Math.random() * ((max - min) + 1)) + min;
+			double Roll = (int) (d1 + d2);
+			// Display the user's roll.
+			System.out.printf("Your roll is a %f", Roll);
+			{
+				if (d1 + d2 == 7)
+				{
+					System.out.println(", Winner");
+				} else
+				{
+					if (d1 + d2 == 11)
+					{
+						System.out.println(", Winner");
+					} else
+					{
+						if (d1 + d2 == 12)
+						{
+							System.out.println(", Jackpot");
+						} else
+						{
+							System.out.println(", Loser");
+						}
+					}
+				}
+			}
+		}
+	}// end of main
+
+}// end of class
