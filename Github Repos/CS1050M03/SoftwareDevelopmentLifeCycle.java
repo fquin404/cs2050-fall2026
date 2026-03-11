9d14daa18f202e9ae7d91f8221d311050d0a041d
fquin404 <corrupted.darkwraith@gmail.com>
2026-03-11 13:32:44 -0600

    meow

--- Github Repos/CS1050M03/SoftwareDevelopmentLifeCycle.java
+++ Github Repos/CS1050M03/SoftwareDevelopmentLifeCycle.java
@@ -4,15 +4,6 @@
  *  Class: CS1050 M/W
  *  Description: Guided Exploration 03
  */
-/**
-Team and Athlete Analysis 
-The trainer enters athlete data until they indicate they are done entering data. The trainer will enter each athlete’s weight and height
-For each athlete entered, the BMI value and category will be displayed based on these BMI ranges.
-Under 18.5: Underweight
-18.5 to under 25: Normal
-25 to under 30: Overweight
-30 or greater: Obese 
- */
 import java.util.Scanner;
 
 public class SoftwareDevelopmentLifeCycle
@@ -27,15 +18,10 @@ public class SoftwareDevelopmentLifeCycle
 
 	}// end of printSummary
 
-	public static void bmiCalculator(String[] args)
+	public void GetPositiveDouble(String[] args)
 	{
-
-		try (// Create a Scanner object for scanning input from keyboard
-				Scanner input = new Scanner(System.in))
+		Scanner input = new Scanner(System.in);
 		{
-			// Tell the user what the program will do.
-			System.out.println("This program will calculate your " + "body mass index, or BMI.");
-
 			// Get the user's weight.
 			System.out.print("Enter your weight, in pounds: ");
 			// ADD CODE to input next double for weight
@@ -44,42 +30,60 @@ public class SoftwareDevelopmentLifeCycle
 			System.out.print("Enter your height, in inches: ");
 			// ADD CODE input next double for height
 			final double height = input.nextDouble();
-			// ADD code for BMI factor
+
+			BMICalculator(weight, height);
+		}
+
+	}// end of GetPositiveDouble
+
+	public static void BMICalculator(double weight, double height)
+	{
+		Scanner input = new Scanner(System.in);
+		{
 			final double BMI_US_FACTOR = 730;
 			// Calculate the user's body mass index.
 			double bmi = weight * BMI_US_FACTOR / (Math.pow(height, 2));
+			displayBMI(bmi);
+		}
+	}
 
-			// Display the user's BMI.
-			System.out.printf("Your body mass index (BMI) is %f", bmi);
+	public static void displayBMI(double bmi)
+	{
+		// Display the user's BMI.
+
+		System.out.printf("Your body mass index (BMI) is %f", bmi);
+		{
+			if (bmi <= 18.5)
 			{
-				if (bmi <= 18.5)
+				System.out.println(", you are Underweight");
+			} else
+			{
+				if (bmi <= 24.9)
 				{
-					System.out.println(", you are Underweight");
+					System.out.println(", you are Healthy");
 				} else
 				{
-					if (bmi <= 24.9)
+					if (bmi <= 29.9)
 					{
-						System.out.println(", you are Healthy");
+						System.out.println(", you are Overweight");
 					} else
 					{
-						if (bmi <= 29.9)
-						{
-							System.out.println(", you are Overweight");
-						} else
-						{
-							if (bmi >= 30)
-								System.out.println(", you are Obese");
-						}
+						if (bmi >= 30)
+							System.out.println(", you are Obese");
 					}
 				}
 			}
 		}
-	}// end of main
+	}
 
-	public void GetPositiveDouble(String[] args)
+	public static boolean AskToContinue()
 	{
-		System.out.println("Meow");
 
-	}// end of GetPositiveDouble
+		return true;
+	}
 
+	public static void main(String[] args)
+	{
+
+	}// end of main
 }// end of class
---
+++ Github Repos/CS1050M04/M04ArraysIntroLab.java
@@ -0,0 +1,15 @@
+public class M04ArraysIntroLab
+{
+	public static void main(String[] args)
+	{
+
+		int[] scores =
+		{ 90, 85, 100 }; // storing multiple values
+
+		System.out.println(scores[0]); // calling 90
+		System.out.println(scores[2]); // calling 100
+
+		scores[1] = 95;
+		System.out.println(scores[1]); // calling 95
+	}
+}
