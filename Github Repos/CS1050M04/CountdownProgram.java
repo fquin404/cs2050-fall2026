c9498990bbc230599e1c5843166d23817c1cfbbd
fquin404 <corrupted.darkwraith@gmail.com>
2026-03-31 13:26:12 -0600

    meow

---
+++ Github Repos/CS1050M04/CountdownProgram.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class CountdownProgram
+{
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+
+		int startNumber = getPositiveInteger(input);
+		displayCountdown(startNumber);
+
+		input.close();
+	}
+
+	public static int getPositiveInteger(Scanner input)
+	{
+		int number;
+
+		while (true)
+		{
+			System.out.print("Enter start number: ");
+
+			if (input.hasNextInt())
+			{
+				number = input.nextInt();
+
+				if (number >= 1)
+				{
+					return number;
+				}
+			} else
+			{
+				input.next();
+			}
+
+			System.out.println("Invalid, try again.");
+		}
+	}
+
+	public static void displayCountdown(int startNumber)
+	{
+		for (int i = startNumber; i >= 1; i--)
+		{
+			System.out.println(i);
+		}
+
+		System.out.println("Blast off!");
+	}
+}
--- Github Repos/CS1050M04/M04ArraysIntroLab.java
+++ Github Repos/CS1050M04/M04ArraysIntroLab.java
@@ -1,15 +1,35 @@
 public class M04ArraysIntroLab
 {
+
 	public static void main(String[] args)
 	{
+		// Array
+		double[] grades =
+		{ 88.11, 95.78, 73.58, 72.00, 25.00 };
 
-		int[] scores =
-		{ 90, 85, 100 }; // storing multiple values
+		int minIndex = findMinIndex(grades);
+		double minGrade = grades[minIndex];
 
-		System.out.println(scores[0]); // calling 90
-		System.out.println(scores[2]); // calling 100
+		System.out.println("Grades array:");
+		for (int i = 0; i < grades.length; i++)
+		{
+			System.out.println("Index " + i + ": " + grades[i]);
+		}
+		System.out.println("Minimum grade: " + minGrade + " at index " + minIndex);
+	}
 
-		scores[1] = 95;
-		System.out.println(scores[1]); // calling 95
+	public static int findMinIndex(double[] grades)
+	{
+		int minIndex = 0;
+		double minValue = grades[0];
+		for (int i = 1; i < grades.length; i++)
+		{
+			if (grades[i] < minValue)
+			{
+				minValue = grades[i];
+				minIndex = i;
+			}
+		}
+		return minIndex;
 	}
 }
