265b93064e8461d44f6f34bb759f8503c557aaf6
fquin404 <corrupted.darkwraith@gmail.com>
2026-08-31 12:39:59 -0600

    mow

--- cs2052/Main.java
+++ cs2052/Main.java
@@ -11,13 +11,27 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		myArrayList mal = new myArrayList(a: 6);
-		mal.add(7);
-		mal.add(5);
+		
+		Dog truffles = new Dog(Name = "Truffles", weight = 25, bark = "Baark!");
+		
+		System.out.println(truffles.getName());
+		System.out.println("truffles weight:" +truffles.getWeight());
+		
+		truffles.getWeight(20);
+		
+		
+		
+		
+		
+		
+		
+		//myArrayList mal = new myArrayList();
+		//mal.add(7);
+		//mal.add(5);
 		//mal.add(4);
 		//mal.add(3);
 		
-		System.out.println(mal);
+		//System.out.println(mal);
 		
 
 	}
