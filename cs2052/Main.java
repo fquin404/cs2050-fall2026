c1876fe30eded8d4196d1f1cc0929d737ec2a4ab
fquin404 <corrupted.darkwraith@gmail.com>
2026-09-02 16:44:03 -0600

    mreow

--- cs2052/Main.java
+++ cs2052/Main.java
@@ -1,39 +1,49 @@
-/**
- * 
- */
+//Francisco Quintero CS2050 M/W Prof. Sasha
 
-/**
- * 
- */
-import java.util.ArrayList;
+public class Main { // creates the main class
 
-public class Main {
+	public static void main(String[] args) { // starts the main method
 
+		myArrayList numbers = new myArrayList(); // creates an empty list for testing
 
-	public static void main(String[] args) {
-		
-		Dog truffles = new Dog(Name = "Truffles", weight = 25, bark = "Baark!");
-		
-		System.out.println(truffles.getName());
-		System.out.println("truffles weight:" +truffles.getWeight());
-		
-		truffles.getWeight(20);
-		
-		
-		
-		
-		
-		
-		
-		//myArrayList mal = new myArrayList();
-		//mal.add(7);
-		//mal.add(5);
-		//mal.add(4);
-		//mal.add(3);
-		
-		//System.out.println(mal);
-		
+		System.out.println("starting list: " + numbers); // prints the empty list
 
-	}
+		numbers.add(1); // adds one to the end of the list
 
-}
+		numbers.add(2); // adds two to the end of the list
+
+		numbers.add(3); // adds three to the end of the list
+
+		numbers.add(4); // adds four to the end of the list
+
+		System.out.println("after growing the array: " + numbers); // prints the list after it grows
+
+		numbers.add(0, 5); // inserts five at the beginning
+
+		System.out.println("after inserting at the beginning: " + numbers); // prints the beginning insert result
+
+		numbers.add(3, 9); // inserts nine in the middle
+
+		System.out.println("after inserting in the middle: " + numbers); // prints the middle insert result
+
+		System.out.println("value at index three: " + numbers.get(3)); // tests the get method
+
+		numbers.remove(0); // removes the first number
+
+		System.out.println("after removing the first value: " + numbers); // prints the first removal result
+
+		numbers.remove(2); // removes a number from the middle
+
+		System.out.println("after removing a middle value: " + numbers); // prints the middle removal result
+
+		numbers.remove(); // removes the last number
+
+		System.out.println("after removing the last value: " + numbers); // prints the last removal result
+
+		myArrayList startingNumber = new myArrayList(7); // creates a list with one starting number
+
+		System.out.println("list made with the second constructor: " + startingNumber); // tests the second constructor
+
+	} // ends the main method
+
+} // ends the main class
--- cs2052/myArrayList.java
+++ cs2052/myArrayList.java
@@ -1,93 +1,78 @@
 //Francisco Quintero CS2050 M/W Prof. Sasha
 
+//DO NOT DELETE KEEP THIS AS AN EXAMPLE / REFERENCE FOR LATER
 
-public class myArrayList { //start of class
+public class myArrayList { // creates the custom array list class
 
-	int intSize = 1; 
+	int intSize = 1; // sets the starting array length to one
 
-	int[] privateArray = new int[intSize]; 
+	int[] privateArray = new int[intSize]; // creates the array that stores the numbers
 
-	int size = 0; 
-
-	public static void main(String[] args) { // start of main method
-
-		myArrayList testList = new myArrayList(); 
-
-		System.out.println("starting list: " + testList); 
-
-		testList.add(1); 
-
-		testList.add(2); 
-
-		testList.add(3); 
-
-		System.out.println("after add: " + testList); 
-
-		testList.add(1, 9); 
-
-		System.out.println("after indexed add: " + testList); 
-
-		System.out.println("number at index two: " + testList.get(2)); 
-
-		testList.remove(); 
-
-		System.out.println("after remove: " + testList); 
-
-		testList.remove(1);
-
-		System.out.println("after indexed remove: " + testList); 
-
-		myArrayList secondList = new myArrayList(7); 
-
-		System.out.println("list made with starting number: " + secondList); 
-
-	} // end of main
+	int size = 0; // keeps track of how many numbers are stored
 
 	public myArrayList(int a) { // creates a list with one starting number
 
 		add(a); // adds the starting number to the list
 
-	} // end of constructor
+	} // ends the constructor
 
 	public myArrayList() { // creates an empty list
 
-	} // end of constructor
+	} // ends the empty constructor
 
 	public void add(int a) { // adds a number to the end of the list
 
 		if (size >= privateArray.length) { // checks if the array is full
 
-			int[] newArray = new int[privateArray.length + 1]; // creates an array with one extra space
+			int[] newArray = new int[privateArray.length + 1]; // creates an array with one more space
 
-			for (int i = 0; i < privateArray.length; i++) { // moves through the previous array
+			for (int i = 0; i < privateArray.length; i++) { // moves through the old array
 
-				newArray[i] = privateArray[i]; // copies number into the new array
+				newArray[i] = privateArray[i]; // copies each number into the new array
 
-			} // end of copy loop
+			} // ends the copying loop
 
 			privateArray = newArray; // replaces the old array with the new array
 
-		}
+		} // ends the full array check
 
 		privateArray[size] = a; // places the number in the next open space
 
 		size++; // increases the number of stored items
 
-	}
+	} // ends the add method
 
-	public void add(int index, int a) { // adds a number at the chosen index
+	public void add(int index, int a) { // adds a number at a chosen index
 
-		add(a); // adds the number and makes more space
+		if (index >= 0 && index <= size) { // checks that the index can be used
 
-		for (int i = size - 1; i > index; i--) { // moves numbers one space to the right
+			if (size >= privateArray.length) { // checks if the array is full
 
-			privateArray[i] = privateArray[i - 1]; // copies the number from the space on the left
+				int[] newArray = new int[privateArray.length + 1]; // creates an array with one more space
 
-		}
+				for (int i = 0; i < privateArray.length; i++) { // moves through the old array
 
-		privateArray[index] = a; // places the new number at the chosen index
+					newArray[i] = privateArray[i]; // copies each number into the new array
 
-	}
+				} // ends the copying loop
+
+				privateArray = newArray; // replaces the old array with the new array
+
+			} // ends the full array check
+
+			for (int i = size; i > index; i--) { // moves later numbers one space to the right
+
+				privateArray[i] = privateArray[i - 1]; // copies the number from the space on the left
+
+			} // ends the shifting loop
+
+			privateArray[index] = a; // places the new number at the chosen index
+
+			size++; // increases the number of stored items
+
+		} // ends the index check
+
+	} // ends the indexed add method
 
 	public void remove() { // removes the last number in the list
 
@@ -95,9 +80,11 @@ public class myArrayList { //start of class
 
 			size--; // lowers the number of stored items by one
 
-		} 
+			privateArray[size] = 0; // clears the unused array space
 
-	}
+		} // ends the empty list check
+
+	} // ends the remove method
 
 	public void remove(int index) { // removes the number at a chosen index
 
@@ -107,19 +94,21 @@ public class myArrayList { //start of class
 
 				privateArray[i] = privateArray[i + 1]; // copies the next number into the current space
 
-			} // end of shifting loop
+			} // ends the shifting loop
 
 			size--; // lowers the number of stored items by one
 
-		} 
+			privateArray[size] = 0; // clears the unused array space
 
-	}
+		} // ends the index check
 
-	public int get(int index) { // gets the number at the chosen index
+	} // ends the indexed remove method
+
+	public int get(int index) { // gets the number at a chosen index
 
 		return privateArray[index]; // returns the number from the chosen index
 
-	} // end of get
+	} // ends the get method
 
 	public String toString() { // changes the list into a string
 
@@ -129,10 +118,10 @@ public class myArrayList { //start of class
 
 			temp = temp + privateArray[i]; // adds each number to the string
 
-		}
+		} // ends the string loop
 
 		return temp; // returns the completed string
 
-	}
+	} // ends the toString method
 
-} // end of class
+} // ends the custom array list class
